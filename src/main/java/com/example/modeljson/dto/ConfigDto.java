4b8eeb213c61a6f95c2284760d80f67edbfd6aab
jucavi <jcarvi@gmail.com>
2024-04-11 12:42:10 +0200

    implement config data object implementation

--- src/main/java/com/example/modeljson/dto/ConfigDto.java
+++ src/main/java/com/example/modeljson/dto/ConfigDto.java
@@ -1,6 +1,27 @@
 package com.example.modeljson.dto;
 
+
 import com.example.modeljson.config.api.utils.AbstractEntityDtoConfig;
 
+import lombok.*;
+
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ConfigDto extends AbstractEntityDtoConfig {
+
+
+    private Long id;
+
+    private String description;
+
+    private String default_value;
+
+    @NonNull()
+    private String attributeId;
+
+    private String parentId;
 }
