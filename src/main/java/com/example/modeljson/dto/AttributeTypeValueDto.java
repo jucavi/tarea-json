dabb1c3173ec925b9ca54b61083d4d81f41045f0
jucavi <jcarvi@gmail.com>
2024-04-11 12:25:44 +0200

    add empty dto class

---
+++ src/main/java/com/example/modeljson/dto/AttributeTypeValueDto.java
@@ -0,0 +1,6 @@
+package com.example.modeljson.dto;
+
+import com.example.modeljson.config.api.utils.AbstractEntityDtoConfig;
+
+public class AttributeTypeValueDto extends AbstractEntityDtoConfig {
+}
