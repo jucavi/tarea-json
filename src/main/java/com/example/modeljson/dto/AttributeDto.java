434f92d7693caf700aa1cedef51d41f45cd3f6ae
jucavi <jcarvi@gmail.com>
2024-04-11 12:44:43 +0200

    implement Attribute class data object implementation

--- src/main/java/com/example/modeljson/dto/AttributeDto.java
+++ src/main/java/com/example/modeljson/dto/AttributeDto.java
@@ -2,5 +2,15 @@ package com.example.modeljson.dto;
 
 import com.example.modeljson.config.api.utils.AbstractEntityDtoConfig;
 
+import javax.validation.constraints.NotNull;
+
 public class AttributeDto extends AbstractEntityDtoConfig {
+
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private Long attributeTypeId;
 }
