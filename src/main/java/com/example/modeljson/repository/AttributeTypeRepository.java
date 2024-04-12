01e0884a5fbf85de6df0bffb4b0ab33e583fb8e5
jucavi <jcarvi@gmail.com>
2024-04-12 10:08:46 +0200

    fix add repository annotation

--- src/main/java/com/example/modeljson/repository/AttributeTypeRepository.java
+++ src/main/java/com/example/modeljson/repository/AttributeTypeRepository.java
@@ -2,6 +2,8 @@ package com.example.modeljson.repository;
 
 import com.example.modeljson.model.AttributeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AttributeTypeRepository extends JpaRepository<AttributeType, Long> {
 }
--- src/main/java/com/example/modeljson/repository/ConfigRepository.java
+++ src/main/java/com/example/modeljson/repository/ConfigRepository.java
@@ -2,6 +2,19 @@ package com.example.modeljson.repository;
 
 import com.example.modeljson.model.Config;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface ConfigRepository extends JpaRepository<Config, Long> {
+
+    /**
+     * Find parent for given parent identifier
+     *
+     * @param parentId parent identifier
+     * @return a list of children for a parent
+     */
+    List<Config> findByParentId(Long parentId);
 }
