283aa25eed4098b204bd7bddd330fde1c973c22e
jucavi <jcarvi@gmail.com>
2024-04-12 13:54:53 +0200

    add global service Interface

---
+++ src/main/java/com/example/modeljson/service/interfaces/Service.java
@@ -0,0 +1,20 @@
+package com.example.modeljson.service.interfaces;
+
+
+import java.util.List;
+
+/**
+ * Service interface
+ * 
+ * @param <S> Data Transfer Object Class 
+ * @param <T> Data Access Object Class
+ */
+public interface Service<S, T> {
+    List<S> findAll();
+    List<S> findDeepAll();
+    S findById(Long id);
+    S findDeepById(Long id);
+    S create(T attributeType);
+    S update(T attributeType);
+    void deleteById(Long id);
+}
