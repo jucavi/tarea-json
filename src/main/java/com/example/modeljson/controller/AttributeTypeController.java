69cd42151a05fa12887b6795132d8318de701d02
jucavi <jcarvi@gmail.com>
2024-04-12 14:01:23 +0200

    add endpoints

--- src/main/java/com/example/modeljson/controller/AttributeTypeController.java
+++ src/main/java/com/example/modeljson/controller/AttributeTypeController.java
@@ -1,7 +1,9 @@
 package com.example.modeljson.controller;
 
 
-import com.example.modeljson.dto.AttributeTypeDto;
+import com.example.modeljson.dto.attributetype.AttributeTypeDto;
+import com.example.modeljson.dto.attributetype.AttributeTypePostDto;
+import com.example.modeljson.dto.attributetype.AttributeTypeUpdateDto;
 import com.example.modeljson.model.AttributeType;
 import com.example.modeljson.service.interfaces.AttributeTypeServiceInterface;
 import io.swagger.v3.oas.annotations.Operation;
@@ -11,7 +13,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -34,10 +35,28 @@ public class AttributeTypeController {
 
 
     /**
+     * Get all attributes names from database even if soft deleted
+     */
+    @GetMapping("deep")
+    @Operation(summary = "All attributes types even if soft deleted", tags = {"GET"}, description = "Retrieve all attributes types from database even if deleted")
+    public ResponseEntity<List<AttributeTypeDto>> findDeepAll() {
+        return ResponseEntity.ok(service.findDeepAll());
+    }
+
+    /**
      * Find an attribute name from database
      */
     @GetMapping("/{id}")
     @Operation(summary = "Get attribute type by ID", tags = {"GET"}, description = "Retrieve an attribute type from database")
+    public ResponseEntity<AttributeTypeDto> findDeepById(@PathVariable Long id) {
+        return ResponseEntity.ok(service.findDeepById(id));
+    }
+
+    /**
+     * Find an attribute name from database
+     */
+    @GetMapping("deep/{id}")
+    @Operation(summary = "Get attribute type by ID even if soft deleted", tags = {"GET"}, description = "Retrieve an attribute type from database even is deleted")
     public ResponseEntity<AttributeTypeDto> findById(@PathVariable Long id) {
         return ResponseEntity.ok(service.findById(id));
     }
@@ -47,10 +66,12 @@ public class AttributeTypeController {
      * Create an attribute name in the database
      */
     @PostMapping
-    public ResponseEntity<AttributeTypeDto> create(@RequestBody @Valid AttributeTypeDto attributeTypeDto) {
+    @Operation(summary = "Create an attribute type", tags = {"POST"}, description = "Create an attribute type")
+    public ResponseEntity<AttributeTypeDto> create(@RequestBody AttributeTypePostDto attributeTypePostDto) {
+
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(service.create(
-                        modelMapper.map(attributeTypeDto, AttributeType.class)));
+                        modelMapper.map(attributeTypePostDto, AttributeType.class)));
     }
 
 
@@ -58,10 +79,12 @@ public class AttributeTypeController {
      * Update an attribute name info
      */
     @PutMapping
-    public ResponseEntity<AttributeTypeDto> update(@RequestBody @Valid AttributeTypeDto attributeTypeDto) {
+    @Operation(summary = "Update an attribute type", tags = {"PUT"}, description = "Update an attribute type")
+    public ResponseEntity<AttributeTypeDto> update(@RequestBody AttributeTypeUpdateDto attributeTypeUpdateDto) {
+
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(service.update(
-                        modelMapper.map(attributeTypeDto, AttributeType.class)));
+                        modelMapper.map(attributeTypeUpdateDto, AttributeType.class)));
     }
 
 
@@ -69,6 +92,7 @@ public class AttributeTypeController {
      * Delete an attribute name from a database
      */
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete an attribute type", tags = {"DELETE"}, description = "Delete an attribute type")
     public ResponseEntity<?> deleteById(@PathVariable Long id) {
         service.deleteById(id);
         return ResponseEntity.ok().build();
