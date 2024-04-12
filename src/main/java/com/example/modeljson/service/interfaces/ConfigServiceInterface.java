270b816c8bc722310b9b11b49f9416c0241bdd97
jucavi <jcarvi@gmail.com>
2024-04-12 14:53:35 +0200

    rename service layer interface

--- src/main/java/com/example/modeljson/service/interfaces/ConfigServiceInterface.java
+++ src/main/java/com/example/modeljson/service/interfaces/ConfigServiceInterface.java
@@ -4,5 +4,5 @@ package com.example.modeljson.service.interfaces;
 import com.example.modeljson.dto.ConfigDto;
 import com.example.modeljson.model.Config;
 
-public interface ConfigServiceInterface extends Service<ConfigDto, Config> {
+public interface ConfigServiceInterface extends ServiceLayerInterface<ConfigDto, Config> {
 }
