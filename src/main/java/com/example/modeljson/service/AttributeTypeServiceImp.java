cfac4672f89e3842619d90fd5459aacba5a9afd9
jucavi <jcarvi@gmail.com>
2024-04-12 14:01:10 +0200

    implement required methods

--- src/main/java/com/example/modeljson/service/AttributeTypeServiceImp.java
+++ src/main/java/com/example/modeljson/service/AttributeTypeServiceImp.java
@@ -1,7 +1,7 @@
 package com.example.modeljson.service;
 
 
-import com.example.modeljson.dto.AttributeTypeDto;
+import com.example.modeljson.dto.attributetype.AttributeTypeDto;
 import com.example.modeljson.error.notfound.AttributeTypeNotFoundException;
 import com.example.modeljson.model.AttributeType;
 import com.example.modeljson.repository.AttributeTypeRepository;
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Service;
 
 import javax.validation.Valid;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 
@@ -33,6 +32,19 @@ public class AttributeTypeServiceImp implements AttributeTypeServiceInterface {
      */
     @Override
     public List<AttributeTypeDto> findAll() {
+
+        log.info("Retrieving all players from database");
+        return repository.findAll()
+                .stream()
+                .filter(attr -> !attr.getDeleted())
+                .map(attr -> modelMapper.map(attr, AttributeTypeDto.class))
+                .collect(Collectors.toList());
+    }
+
+
+    @Override
+    public List<AttributeTypeDto> findDeepAll() {
+
         log.info("Retrieving all players from database");
         return repository.findAll()
                 .stream()
@@ -50,9 +62,25 @@ public class AttributeTypeServiceImp implements AttributeTypeServiceInterface {
      */
     @Override
     public AttributeTypeDto findById(@NonNull Long id) throws AttributeTypeNotFoundException {
+
         log.info("Retrieving attribute type with id: {}", id);
 
-        Optional<AttributeType> attributeType = repository.findById(id);
+        var attributeType = repository.findById(id);
+
+        if (attributeType.isEmpty() || attributeType.get().getDeleted()) {
+            throw new AttributeTypeNotFoundException("Attribute type not found.");
+        }
+
+        return modelMapper.map(attributeType.get(), AttributeTypeDto.class);
+    }
+
+
+    @Override
+    public AttributeTypeDto findDeepById(Long id) {
+
+        log.info("Retrieving attribute type with id: {}", id);
+
+        var attributeType = repository.findById(id);
 
         if (attributeType.isEmpty()) {
             throw new AttributeTypeNotFoundException("Attribute type not found.");
@@ -71,6 +99,7 @@ public class AttributeTypeServiceImp implements AttributeTypeServiceInterface {
      */
     @Override
     public AttributeTypeDto create(@Valid AttributeType attributeType) throws  RuntimeException {
+
         log.info("Creating attribute type");
 
         if (attributeType.getId() != null) {
@@ -83,37 +112,55 @@ public class AttributeTypeServiceImp implements AttributeTypeServiceInterface {
             log.info("Attribute type '{}' created with ID: {}", result, result.getId());
 
         } catch (Exception ex) {
-            log.error(ex.getMessage());
-            throw new RuntimeException("Unable to create a attribute type.");
+            throw new RuntimeException("Unable to create attribute type.");
         }
+
         return modelMapper.map(result, AttributeTypeDto.class);
     }
 
+
     /**
      * Update an attribute type for the given id
+     *
+     * <p>
+     * Only enum description can be updated.
+     * </p>
+     *
      * @param attributeType attribute type
      * @return attribute type dto
      * @throws RuntimeException raise exception if trying to create attribute type with null id
      */
     @Override
-    public AttributeTypeDto update(@Valid AttributeType attributeType) throws  RuntimeException {
+    public AttributeTypeDto update(AttributeType attributeType) throws  RuntimeException {
+
         log.info("Updating attribute type");
 
         if (attributeType.getId() == null) {
-            throw new RuntimeException("Trying to update an attribute type, but ID not null.");
+            throw new RuntimeException("Trying to update attribute type, but ID not null.");
         }
+
+        var oldEntityOp = repository.findById(attributeType.getId());
+
+        if (oldEntityOp.isEmpty()) {
+            throw new AttributeTypeNotFoundException("Attribute type entity not found");
+        }
+
+        var oldEntity = oldEntityOp.get();
+        oldEntity.setEnumDescription(attributeType.getEnumDescription());
+        oldEntity.setDeleted(attributeType.getDeleted());
 
         AttributeType result;
         try {
-            result = repository.save(attributeType);
-            log.info("Attribute type '{}' created with ID: {}", result, result.getId());
+            result = repository.save(oldEntity);
+            log.info("Attribute type '{}' updated.", result);
 
         } catch (Exception ex) {
-            log.error(ex.getMessage());
-            throw new RuntimeException("Unable to create a attribute type.");
+            throw new RuntimeException("Unable to update attribute type.");
         }
+
         return modelMapper.map(result, AttributeTypeDto.class);
     }
+
 
     /**
      * Remove an attribute type from database with the given id
@@ -121,7 +168,17 @@ public class AttributeTypeServiceImp implements AttributeTypeServiceInterface {
      */
     @Override
     public void deleteById(Long id) {
-        log.info("Deleting attribute type with ID: {}", id);
-        repository.deleteById(id);
+        log.info("Soft deleting attribute type with ID: {}", id);
+
+        var entityOp = repository.findById(id);
+
+        if (entityOp.isEmpty()) {
+            throw new AttributeTypeNotFoundException("Attribute type entity not found");
+        }
+
+        var entity = entityOp.get();
+        entity.setDeleted(true);
+
+        repository.save(entity);
     }
 }
