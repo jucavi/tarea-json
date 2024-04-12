64ff6bdf9c6bff1ea29a2dd9ec087c972148b541
jucavi <jcarvi@gmail.com>
2024-04-12 13:58:09 +0200

    add attribute type dtos for POST and PUT

---
+++ src/main/java/com/example/modeljson/dto/attributetype/AttributeTypePostDto.java
@@ -0,0 +1,25 @@
+package com.example.modeljson.dto.attributetype;
+
+import com.example.modeljson.config.api.utils.AbstractEntityDtoConfig;
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AttributeTypePostDto extends AbstractEntityDtoConfig {
+
+    @NotNull
+    @NotBlank(message = "Type field can't be empty")
+    private String type;
+
+    private String enumDescription = "";
+
+    private Boolean isEnum = Boolean.FALSE;
+
+    private Boolean isList = Boolean.FALSE;
+}
---
+++ src/main/java/com/example/modeljson/dto/attributetype/AttributeTypeUpdateDto.java
@@ -0,0 +1,16 @@
+package com.example.modeljson.dto.attributetype;
+
+import com.example.modeljson.config.api.utils.AbstractEntityDtoConfig;
+import lombok.*;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+public class AttributeTypeUpdateDto extends AbstractEntityDtoConfig {
+
+    private Long id;
+
+    private String enumDescription;
+
+    private boolean deleted = Boolean.FALSE;
+}
