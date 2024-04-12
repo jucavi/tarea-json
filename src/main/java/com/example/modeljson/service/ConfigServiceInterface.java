ac4629016d9c5c750fb51a0eb0216a29f68dc31e
jucavi <jcarvi@gmail.com>
2024-04-12 09:14:23 +0200

    add DRUD config service interface

---
+++ src/main/java/com/example/modeljson/service/ConfigServiceInterface.java
@@ -0,0 +1,16 @@
+package com.example.modeljson.service;
+
+
+import com.example.modeljson.dto.ConfigDto;
+import com.example.modeljson.model.Config;
+
+import java.util.List;
+
+public interface ConfigServiceInterface {
+
+    List<ConfigDto> findAll();
+    ConfigDto findById(Long id);
+    ConfigDto create(Config configType);
+    ConfigDto update(Config configType);
+    void deleteById(Long id);
+}
