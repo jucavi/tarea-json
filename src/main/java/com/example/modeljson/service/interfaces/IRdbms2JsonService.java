ae75c34d054bae44de343b90ebede293b5cb23a5
Jucavi <jcarvil@gmail.com>
2024-04-14 01:05:30 +0200

    rename methods

--- src/main/java/com/example/modeljson/service/interfaces/IRdbms2JsonService.java
+++ src/main/java/com/example/modeljson/service/interfaces/IRdbms2JsonService.java
@@ -6,7 +6,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface IRdbms2JsonService {
 
-    void storeJson(@NonNull MultipartFile file);
+    void storeConfigJson(@NonNull MultipartFile file);
 
-    JsonNode buildJson();
+    JsonNode buildConfigJson();
 }
