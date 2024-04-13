f10b13054fb225798d1d492ce417e9c58ec031f8
Jucavi <jcarvil@gmail.com>
2024-04-14 00:52:20 +0200

    rename services implementations

--- src/main/java/com/example/modeljson/service/AttributeTypeServiceImpl.java
+++ src/main/java/com/example/modeljson/service/AttributeTypeServiceImpl.java
@@ -19,7 +19,7 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 @Log4j2
-public class AttributeTypeServiceImp implements IAttributeTypeService {
+public class AttributeTypeServiceImpl implements IAttributeTypeService {
 
     private final IAttributeTypeRepository repository;
     private final ModelMapper modelMapper;
--- src/main/java/com/example/modeljson/service/ConfigServiceImpl.java
+++ src/main/java/com/example/modeljson/service/ConfigServiceImpl.java
@@ -20,7 +20,7 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 @Log4j2
-public class ConfigServiceImp implements IConfigService {
+public class ConfigServiceImpl implements IConfigService {
 
     private final IConfigRepository repository;
 
