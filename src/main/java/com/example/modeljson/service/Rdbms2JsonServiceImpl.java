d1fbd46cee3b439ab290a7f6865578ebeedda850
Jucavi <jcarvil@gmail.com>
2024-04-14 22:37:32 +0200

    add traverse method

--- src/main/java/com/example/modeljson/service/Rdbms2JsonServiceImpl.java
+++ src/main/java/com/example/modeljson/service/Rdbms2JsonServiceImpl.java
@@ -1,5 +1,8 @@
 package com.example.modeljson.service;
 
+import com.example.modeljson.model.Attribute;
+import com.example.modeljson.model.AttributeType;
+import com.example.modeljson.model.Config;
 import com.example.modeljson.repository.IAttributeRepository;
 import com.example.modeljson.repository.IAttributeTypeRepository;
 import com.example.modeljson.repository.IAttributeTypeValueRepository;
@@ -7,17 +10,20 @@ import com.example.modeljson.repository.IConfigRepository;
 import com.example.modeljson.service.interfaces.IRdbms2JsonService;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.JsonNodeType;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -31,26 +37,164 @@ public class Rdbms2JsonServiceImpl implements IRdbms2JsonService {
 
     @Override
     public void storeConfigJson(@NonNull MultipartFile file) {
-        ObjectMapper mapper = new ObjectMapper();
+
+        InputStreamReader reader;
+        JsonNode rootNode;
+
         try {
-            InputStreamReader reader = new InputStreamReader(file.getInputStream());
-
-            JsonNode rootNode = mapper.readTree(reader);
-
-            var fields = rootNode.fields();
-
-            while (fields.hasNext()) {
-                var field = fields.next();
-                System.out.println(fields.next());
-            }
+            ObjectMapper mapper = new ObjectMapper();
+            reader = new InputStreamReader(file.getInputStream());
+            rootNode = mapper.readTree(reader);
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+        traverse(null, rootNode);
     }
 
     @Override
     public JsonNode buildConfigJson() {
+        throw new RuntimeException("Not implemented yet");
+    }
+
+    private void traverse(Config parent, JsonNode root) {
+
+        Iterator<Map.Entry<String, JsonNode>> children = root.fields();
+
+        while(children.hasNext()) {
+
+            Map.Entry<String, JsonNode> child = children.next();
+            String attributeName = child.getKey();
+            JsonNode defaultValue = child.getValue();
+
+            if (defaultValue.isObject()) {
+
+                Optional<Attribute> attributeOpt = attributeRepository.findByName(attributeName);
+
+                Attribute attribute;
+                // Create attribute
+                if (attributeOpt.isEmpty()) {
+
+                    AttributeType attrType = new AttributeType();
+                    attrType.setType(Object.class.getSimpleName());
+
+                    AttributeType attributeType = attributeTypeRepository.save(attrType);
+
+                    Attribute attr = new Attribute();
+                    attr.setName(attributeName);
+                    attr.setAttributeType(attributeType);
+
+                    attribute = attributeRepository.save(attr);
+                } else {
+                    attribute = attributeOpt.get();
+                }
+
+
+                Config config = new Config();
+                config.setParent(parent);
+                config.setAttribute(attribute);
+
+                // store config
+                configRepository.save(config);
+                traverse(config, defaultValue);
+
+            } else if (defaultValue.isArray()) {
+                // TODO: ARRAY
+                System.out.println("is Array");
+                ArrayNode arrayNode = (ArrayNode) defaultValue;
+
+                for (int i = 0; i < arrayNode.size(); i++) {
+                    JsonNode arrayElement = arrayNode.get(i);
+                    System.out.println("ArrayElement[" + i + "] = " + arrayElement);
+                }
+            } else {
+                // TODO: ---- same as Object
+                Optional<Attribute> attributeOpt = attributeRepository.findByName(attributeName);
+
+                Attribute attribute;
+                // Create attribute
+                if (attributeOpt.isEmpty()) {
+                    AttributeType attrType = new AttributeType();
+                    // TODO
+                    attrType.setType("-----------------------------");
+
+                    AttributeType attributeType = attributeTypeRepository.save(attrType);
+
+                    Attribute attr = new Attribute();
+                    attr.setName(attributeName);
+                    attr.setAttributeType(attributeType);
+
+                    attribute = attributeRepository.save(attr);
+                } else {
+                    attribute = attributeOpt.get();
+                }
+
+
+                Config config = new Config();
+                config.setParent(parent);
+                config.setAttribute(attribute);
+
+                // store config
+                configRepository.save(config);
+                // TODO: ---- same as Object
+            }
+        }
+    }
+
+    private AttributeType getAttributeType(JsonNode node) {
+        JsonNodeType fieldType  = node.getNodeType();
+
+
+
+        //boolean isList = type == "List";
+        //boolean isEnum = type == "List";
+
         return null;
+    }
+
+
+    private AttributeType getNodeType(JsonNode node) {
+        String type;
+        boolean isList = false;
+        boolean isEnum = false;
+        JsonNodeType fieldType  = node.getNodeType();
+
+        switch(fieldType) {
+            case OBJECT: {
+                type = Object.class.getSimpleName();
+                break;
+            }
+            case ARRAY: {
+                type = List.class.getSimpleName();
+                isList = true;
+                break;
+            }
+            case BOOLEAN: {
+                type = Boolean.class.getSimpleName();
+                break;
+            }
+            case NUMBER: {
+                if (node.isInt()) {
+                    type = Integer.class.getSimpleName();
+                } else if (node.isDouble()) {
+                    type = Double.class.getSimpleName();
+                }
+                break;
+            }
+            case STRING: {
+                Iterator<Map.Entry<String, JsonNode>> children = node.fields();
+
+                while (children.hasNext()) {
+                    System.out.println("Check getNodeType for enum value" + children.next().getValue());
+                }
+
+                type = String.class.getSimpleName();
+                break;
+            }
+            default: type = null;
+        }
+
+        return new AttributeType();
     }
 }
